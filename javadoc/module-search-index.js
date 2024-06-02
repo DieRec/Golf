e13088c81f1499e45d24e549f2b5801cfe7137d4
Recinos <104780516+diegoRecinos@users.noreply.github.com>
2024-06-02 02:55:52 -0600

    Delete javadoc directory

--- javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"ModularIDEJavafxGolf"}];updateSearchResults();
--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ModularIDEJavafxGolf","l":"juego"},{"m":"ModularIDEJavafxGolf","l":"lvl"}];updateSearchResults();
